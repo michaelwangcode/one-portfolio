18121a1b4fe0d93f228a92a5d74ea645097f018f
michaelwangcode <michaelwangxv@gmail.com>
2023-01-30 09:36:22 -0500

    Bug Fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
   return (
 
     /* Gradient Background on Home Page */
-    <div name="home" className="h-fit w-full bg-gradient-to-b from-black via-black to-gray-800">
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
 
       {/* Container for Home Page Words and Images */}
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
